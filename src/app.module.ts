ec61121607bd1745e339c82b438adf19d4a2dd7b
Soyamstha <shrestha.soyam111@gmail.com>
2026-01-19 12:08:01 +0545

    learning crud in nest js

---
+++ src/app.module.ts
@@ -0,0 +1,23 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { UserModule } from './user/user.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+@Module({
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: 'fzr2010r15',
+      database: 'nest_learning1',
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      synchronize: true, // ❗ only for development
+    }), UserModule],
+  controllers: [AppController],
+  providers: [AppService],
+
+})
+export class AppModule { }
